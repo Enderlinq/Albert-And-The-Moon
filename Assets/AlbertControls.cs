2c804cfe63f5a57f64ee9a1331989b69a662fa24
Enderlinq <wahj060@gmail.com>
2019-07-07 22:16:34 +0300

    Added Terrain + Temporary Wall To Stop Albert From Falling

---
+++ Assets/AlbertControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbertControls : MonoBehaviour
+{
+    Rigidbody AlbertRB;
+    void Start()
+    {
+        AlbertRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotation();
+
+        void Thrust()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                AlbertRB.AddRelativeForce(Vector3.up);
+            }
+        }
+
+        void Rotation()
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(Vector3.back);
+            }
+
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.forward);
+            }
+
+        }
+        
+    }
+
+}
